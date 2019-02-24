538fcc80034a584429c86327b41780da1e743f5b
KingMhamad <42966157+KingMhamad@users.noreply.github.com>
2019-02-24 21:57:24 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,17 +1,65 @@
-print(" |--------------------------------------| ")
-print(" | my first code i think it's a code :D |")
-print(" |--------------------------------------| ")
-try:
-	num1 = int(input("choose a number: "))
-	op = input("choose an operator: ")
-	num2 = int(input("choose another number: "))
-	if op == "+":
-		print(num1 + num2)
-	elif op == "-":
-		print(num1 - num2)
-	elif op == "*":
-		print(num1 * num2)
-	elif op == "/":
-		print(num1 / num2)
-except:
-	print("that's not right")
+def calculator():
+
+	try:		while True:
+
+			print("                      |------------|")
+
+			print("                      | Calculator |")
+
+			print("                      |------------|")
+
+			num1 = float(input("Enter a number: "))
+
+			print("""
+
+Operators:
+
++
+
+-
+
+*
+
+/
+
+""")
+
+			op = input("Enter an operator: ")
+
+			num2 = float(input("Enter another number: "))
+
+			if op == "+":
+
+				print(num1+num2)
+
+			elif op == "-":
+
+				print(num1-num2)
+
+			elif op == "*":
+
+				print(num1*num2)
+
+			elif op ==  "/":
+
+				print(num1/num2)
+
+			elif op == "+" or "-" or "*" or "/":
+
+				print("not a valid operator")
+
+	except ZeroDivisionError:
+
+		print("you can't divide by 0")
+
+		calculator()
+
+	except ValueError:
+
+		print("A NUMBER")
+
+		calculator()
+
+	
+
+calculator()
